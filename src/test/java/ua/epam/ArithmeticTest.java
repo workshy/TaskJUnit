f1a3ae134f5c05ca5d95a3b5ceafad0d0a8660f3
Viktoriia <Viktoriia_Marynchak@epam.com>
2016-09-13 20:03:11 +0300

    Added changes to ArithmeticTest.java. Added method runT().

--- src/test/java/ua/epam/ArithmeticTest.java
+++ src/test/java/ua/epam/ArithmeticTest.java
@@ -9,32 +9,40 @@ import static org.junit.Assert.*;
  * Created by Viktoriia_Marynchak on 9/13/2016.
  */
 public class ArithmeticTest {
+    private Arithmetic a;
+    public void runT() {
+        a = new Arithmetic();
+    }
     @Test
     public void testAdd() throws Exception {
-        Arithmetic a = new Arithmetic();
+        runT();
         double res = a.add(3, 7);
         Assert.assertEquals(10, res, 0);
+        a = null;
     }
 
     @Test
     public void testMult() throws Exception {
-        Arithmetic a = new Arithmetic();
+        runT();
         double res = a.mult(3, 7);
         Assert.assertEquals(21, res, 0);
+        a = null;
     }
 
     @Test
     public void testDeduct() throws Exception {
-        Arithmetic a = new Arithmetic();
+        runT();
         double res = a.deduct(3, 7);
         Assert.assertEquals(-4, res, 0);
+        a = null;
     }
 
     @Test
     public void testDiv() throws Exception {
-        Arithmetic a = new Arithmetic();
+        runT();
         double res = a.div(10, 5);
         Assert.assertEquals(2.0, res, 0);
+        a = null;
     }
 
 }
